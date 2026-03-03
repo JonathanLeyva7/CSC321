c004570ca13ce3f28297b55e812a5b3ed9fc4466
JL <jonathansocialife@gmail.com>
2026-03-03 21:14:56 +0000

    lab7

---
+++ JLeyva7.py
@@ -0,0 +1,27 @@
+#Jonathan Leyva
+#CSC321
+#LAB 7
+
+x = 5;
+
+if x > 5:
+    print("x is greater than 5")
+elif x < 5:
+    print("x is less than 5")
+else: 
+    print("x is ", x ,"which is equal to 5")
+
+y = "strings"
+
+if y == "String":
+    print("y is a string, name String")
+elif y == "strings":
+    print("y is a string, name strings")
+
+z = 7.5;
+
+if z > 5.5:
+    print("z is greater than 5.5")
+elif z < 5.5:
+    print("z is less than ")
+
